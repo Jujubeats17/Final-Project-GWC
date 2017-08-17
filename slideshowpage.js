7b2c2ed16370e690e6763a9ea698c43963c70575
Jujubeats17 <justine.peart@gmail.com>
2017-08-17 13:27:02 -0400

    Update slideshowpage.js

--- slideshowpage.js
+++ slideshowpage.js
@@ -1,23 +1,34 @@
+//function changePicture() {
+  //document.getElementById("cute-animal1").src="cute-animals2.jpg"
+  //document.getElementById("cute-animals1").src="cute-animals3.jpg"
+//}
+
 function showPicture() {
-  document.getElementById("cute-animal1").style.display='block';
+  document.getElementById("cute-animal1").style.display= 'block'
+  document.getElementById("cute-animal1").src = "cute-animals.jpg"
 }
 
 function showPicture2() {
+  document.getElementById("cute-animal1").style.display= 'block'
   document.getElementById("cute-animal1").src ="cute-animals2.jpg";
 }
 
 function showPicture3() {
+  document.getElementById("cute-animal1").style.display= 'block'
   document.getElementById("cute-animal1").src="cute-animals3.jpg";
 }
 
 function showLandscape() {
   document.getElementById("natural-landscape1").style.display='block';
+  document.getElementById("natural-landscape1").src= "natural-landscape.jpg";
 }
 
 function showLandscape2() {
+  document.getElementById("natural-landscape1").style.display='block';
   document.getElementById("natural-landscape1").src= "natural-landscape2.jpg";
 }
 
 function showLandscape3() {
+  document.getElementById("natural-landscape1").style.display='block';
   document.getElementById("natural-landscape1").src="natural-landscape3.jpg";
 }
