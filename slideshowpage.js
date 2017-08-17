7d625791617b1c928da41530c8362160ea61bd1e
Jujubeats17 <justine.peart@gmail.com>
2017-08-17 15:29:23 -0400

    Update slideshowpage.js

--- slideshowpage.js
+++ slideshowpage.js
@@ -7,28 +7,48 @@ function showPicture() {
   document.getElementById("cute-animal1").style.display= 'block'
   document.getElementById("cute-animal1").src = "cute-animals.jpg"
 }
-
 function showPicture2() {
   document.getElementById("cute-animal1").style.display= 'block'
   document.getElementById("cute-animal1").src ="cute-animals2.jpg";
 }
-
 function showPicture3() {
   document.getElementById("cute-animal1").style.display= 'block'
   document.getElementById("cute-animal1").src="cute-animals3.jpg";
+}
+function showPicture4() {
+  document.getElementById("cute-animal1").style.display= 'block'
+  document.getElementById("cute-animal1").src="cute-animals4.jpg";
+}
+function showPicture5()  {
+  document.getElementById("cute-animal1").style.display= 'block'
+  document.getElementById("cute-animal1").src="cute-animals5.jpg";
+}
+function showPicture6() {
+  document.getElementById("cute-animal1").style.display= 'block'
+  document.getElementById("cute-animal1").src="cute-animals6.jpg";
 }
 
 function showLandscape() {
   document.getElementById("natural-landscape1").style.display='block';
   document.getElementById("natural-landscape1").src= "natural-landscape.jpg";
 }
-
 function showLandscape2() {
   document.getElementById("natural-landscape1").style.display='block';
   document.getElementById("natural-landscape1").src= "natural-landscape2.jpg";
 }
-
 function showLandscape3() {
   document.getElementById("natural-landscape1").style.display='block';
   document.getElementById("natural-landscape1").src="natural-landscape3.jpg";
+}
+function showLandscape4() {
+  document.getElementById("natural-landscape1").style.display='block';
+  document.getElementById("natural-landscape1").src="natural-landscape4.jpg";
+}
+function showLandscape5() {
+  document.getElementById("natural-landscape1").style.display='block';
+  document.getElementById("natural-landscape1").src="natural-landscape5.jpg";
+}
+function showLandscape6() {
+  document.getElementById("natural-landscape1").style.display='block';
+  document.getElementById("natural-landscape1").src="natural-landscape6.jpg";
 }
