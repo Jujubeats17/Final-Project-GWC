823b551a9222b593bbcf4399c17d799aad09afe2
CodingGirl5419 <alexandrab910@gmail.com>
2017-08-18 15:48:13 -0400

    Update breathing exercise.js

--- breathing exercise.js
+++ breathing exercise.js
@@ -15,14 +15,14 @@
 //}
 
 function Inhale() {
-  document.getElementById("bubble").style.width = "400px".height = "400px"
+  document.getElementById("bubble").style.width = "400px".height = "400px";
 }
 
 function Exhale() {
-  document.getElementById("bubble").style.width = "204px".height = "204px"
+  document.getElementById("bubble").style.width = "204px".height = "204px";
 }
 
-var myAudio = document.getElementById("rain")
+var myAudio = document.getElementById("rain");
 function togglePlay() {
   if (rain.paused){
     rain.play();
@@ -31,7 +31,7 @@ function togglePlay() {
   }
 };
 
-var myAudio2 = document.getElementById("sea")
+var myAudio2 = document.getElementById("sea");
 function togglePlay2() {
   if (sea.paused){
     sea.play();
@@ -40,11 +40,29 @@ function togglePlay2() {
   }
 };
 
-var myAudio3 = document.getElementById("fire")
+var myAudio3 = document.getElementById("fire");
 function togglePlay3() {
   if (fire.paused){
     fire.play();
   } else {
     fire.pause();
+  }
+};
+
+var myAudio4 = document.getElementById("nightingales");
+function togglePlay4() {
+  if (nightingales.paused){
+    nightingales.play();
+  } else {
+    nightingales.pause();
+  }
+};
+
+var myAudio5 = document.getElementById("creek");
+function togglePlay5() {
+  if (creek.paused){
+    creek.play();
+  } else {
+    creek.pause();
   }
 };
